47821686761d5dee103f9f5a909dd5a94f9eb27e
cyr17 <73247453+cyr17@users.noreply.github.com>
2024-08-08 15:38:06 +1000

    tweaking response headers

--- server/server.js
+++ server/server.js
@@ -45,6 +45,13 @@ const userVotes = {};
 // Define routes
 app.options('*', cors(corsOptions)); // preflight requests
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins, change '*' to specific origin if needed
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 
 app.get('/',(req,res)=>{
   res.send('Connected to Server!');
