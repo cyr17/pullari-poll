f7be2b77e50cdc4229239b71ac7e9e9a186b47f6
cyr17 <73247453+cyr17@users.noreply.github.com>
2024-09-02 21:15:02 +1000

    tweak

--- src/landingPage.js
+++ src/landingPage.js
@@ -33,7 +33,7 @@ const LandingPage = () => {
           <div class="text-center text-white">
               <h1 class="text-lg font-semibold">Pulari Victoria Inc</h1>
               <p class="mt-2">PRESENTS</p>
-              <h2 class="text-2xl font-bold mt-4">Pulari Short Film Awards Ceremony</h2>
+              <h2 class="text-2xl font-bold mt-4">Pulari Short Film Festival Awards Ceremony</h2>
               <div class="mt-6 flex flex-col items-center space-y-4">
                   <div class="flex items-center space-x-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
